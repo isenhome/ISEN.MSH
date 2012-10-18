879f6f426434210a1765b8adf422c237fa23c9c7
isenhome <isenhome@126.com>
2012-10-18 16:47:10 +0800

    completed fullcalendar, facebox and datepicker
    add work , delete work, drag work, edit work ,resize work has completed

---
+++ ISEN.MSH.MODELS/JsonModels/Work.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISEN.MSH.Nhibernate.Models.JsonModels
+{
+    public class Work
+    {
+        public string idcode { get; set; }
+        public string title { get; set; }
+        public string isallday { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+        public string remindtime { get; set; }
+        public string isremind { get; set; }
+        public string content { get; set; }
+    }
+}
--- ISEN.MSH.MVC.Controllers/AdminController/CalendarController.cs
+++ ISEN.MSH.MVC.Controllers/AdminController/CalendarController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using ISEN.MSH.MVC.Controllers.Filters;
+using ISEN.MSH.Nhibernate.Models.JsonModels;
 
 namespace ISEN.MSH.MVC.Controllers.AdminController
 {
@@ -13,6 +14,47 @@ namespace ISEN.MSH.MVC.Controllers.AdminController
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult SubmitWork(Work work)
+        {
+            if (work.idcode == null)
+            {
+                work.idcode = Guid.NewGuid().ToString();
+            }
+            var result = new { result_code = 200, success = true, data = new { work } };
+            return Json(result);
+        }
+
+        [HttpGet]
+        public JsonResult GetWork(Work data)
+        {
+            Work work = new Work();
+            work.idcode = data.idcode;
+            work.title = data.title;
+            work.isallday = data.isallday;
+            work.start =data.start;
+            work.end = data.end;
+            work.remindtime = "15";
+            work.isremind = "true";
+            work.content = "new work content";
+            var result = new { result_code = 200, success = true, data = new { work } };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult RemoveWork(Work work)
+        {
+            var result = new { result_code = 200, success = true, data = new { work } };
+            return Json(result);
+        }
+
+        [HttpPost]
+        public JsonResult ResizeWork(Work work)
+        {
+            var result = new { result_code = 200, success = true, data = new { work } };
+            return Json(result);
         }
     }
 }
