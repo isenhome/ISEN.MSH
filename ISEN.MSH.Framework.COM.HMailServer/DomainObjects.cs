8caf7ef92952257a4bfec1584570f96e5a3906c2
isenhome <isenhome>
2012-11-01 17:11:47 +0800

    add framework.com.hmailserver
    in this project,i want to complete using hmailserver by com+.
    now attempt to coding com+.

---
+++ ISEN.MSH.Framework.COM.HMailServer/DomainObjects.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISEN.MSH.Framework.COM.HMailServer
+{
+    public class DomainObjects
+    {
+        
+    }
+}
