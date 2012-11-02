17bac837e59e401571b471e5a93f5441939e9e6f
isenhome <isenhome>
2012-11-02 17:30:43 +0800

    windows service with WCF
    resource has found.

---
+++ ISEN.MSH.App.WCF.Hmail/HmailWCFService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.ServiceModel;
+using ISEN.MSH.Framework.Mail.Hmail.Service;
+
+namespace ISEN.MSH.App.WCF.Hmail
+{
+    public partial class HmailWCFService : ServiceBase
+    {
+        public ServiceHost serviceHost = null;
+
+        public HmailWCFService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            // Create a ServiceHost for the CalculatorService type and 
+            // provide the base address.
+            serviceHost = new ServiceHost(typeof(Accounts));
+
+            // Open the ServiceHostBase to create listeners and start 
+            // listening for messages.
+            serviceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+        //http://technet.microsoft.com/zh-cn/library/ms733069(v=vs.110)
+        //http://www.cnblogs.com/zengle_love/archive/2009/03/22/1419138.html
+        //http://hi.baidu.com/zkbob22/item/eb908d0a5808d3046c90488f
+        //http://www.hmailserver.com/documentation/v5.3/?page=com_objects
+    }
+}
--- ISEN.MSH.Framework.Mail.Hmail/Interface/IAccounts.cs
+++ ISEN.MSH.Framework.Mail.Hmail/Interface/IAccounts.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceModel;
 
 namespace ISEN.MSH.Framework.Mail.Hmail.Interface
 {
     public interface IAccounts
     {
+        [OperationContract]
         void AddAccount(string address,string password);
-       
+
+        [OperationContract]
         void ChangePassword(string address, string password);
 
+        [OperationContract]
         void DeleteAccount(string address);
     }
 }
