a12206ba71b9464a5a8458f12f6b1733347d0b03
isenhome <isenhome>
2013-03-04 15:27:23 +0800

    add workflow desginer which is downloaded in csdn

--- ISEN.MSH.MVC.Controllers/AdminController/JqueryTableController.cs
+++ ISEN.MSH.MVC.Controllers/AdminController/JqueryTableController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
+using ISEN.MSH.MVC.Controllers.Filters;
 
 namespace ISEN.MSH.MVC.Controllers.AdminController
 {
+    [UserActionFilter]
     public class JqueryTableController : BaseController
     {
         public ActionResult Index()
